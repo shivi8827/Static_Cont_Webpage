696d333d2eb4ea01b125f391cc05f09b4f10878f
shivani tomar <80258536+shivi8827@users.noreply.github.com>
2021-10-08 13:03:19 +0530

    Add files via upload

---
+++ firstproject/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+
+   
+    
+       
+    return render(request,'index.html')
+
+    
+
+def about(request):
+    return render(request,'about.html')
+    
+
+def contact(request):
+
+
+    return render(request,'contact.html')
